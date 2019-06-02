327869223ad5f0ee5c1c92a2ee2309c7fd95e9cd
dorara <satosinisemono@gmail.com>
2019-06-02 15:58:00 +0900

    [add] js機能ほぼ実装

--- src/App.js
+++ src/App.js
@@ -3,10 +3,94 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen : false,
+      koalaSaying :'ねむい…'
+    };
+  }
+
+  random(list) {
+    const randNum = Math.floor(Math.random()* list.length );
+    return(list[randNum]);
+  }
+
+  handleClick(helloType) {
+    let koalaSayingList;
+    switch (helloType) {
+      case 1:
+        koalaSayingList = [
+          'おはコアラ',
+          'おはドアラ',
+          'zzz…',
+          'おはにゃん'
+        ];
+        break;
+      case 2:
+        koalaSayingList = [
+          'こんにちコアラ',
+          'お腹すいた…',
+          'こんちくわ',
+          'コアラのマーチ'
+        ];
+        break;
+      case 3:
+        koalaSayingList = [
+          'おやすみコアラ',
+          'くそねみコアラ',
+          'zzz…（もう寝ているようだ'
+        ];
+        break;
+      default:
+        koalaSayingList = [];
+    }
+    //ランダムにする
+    const koalaSaying = this.random(koalaSayingList);
+
+    this.setState({
+      koalaSaying: koalaSaying,
+      isModalOpen: true
+    });
+  }
+  
   render() {
+    let htmlModal;
+    if (this.state.isModalOpen) {
+      htmlModal = (
+        <div className="modal">
+          <p className="modal-text">コアラ「{this.state.koalaSaying}」</p>
+          <button onClick={() => {this.setState({isModalOpen: false});}}>閉じる</button>
+        </div>
+      );
+    }
     return (
       <div className="App">
-        <h1>Hello Koala!</h1>
+        <h1>コアラに挨拶しよう！</h1>
+        <div className="btn-wrap">
+          <button 
+            onClick={
+              () => {
+                this.handleClick(1)
+              }
+            }
+          >おはよう</button>
+          <button 
+            onClick={
+              () => {
+                this.handleClick(2)
+              }
+            }
+          >こんにちは</button>
+          <button 
+            onClick={
+              () => {
+                this.handleClick(3)
+              }
+            }
+          >おやすみ</button>
+        </div>
+        {htmlModal}
         <footer className="App-footer">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
