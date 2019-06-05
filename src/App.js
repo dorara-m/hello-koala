7b1826c59c35613e5ecb14b05afca14428c84788
dorara <satosinisemono@gmail.com>
2019-06-06 00:32:10 +0900

    [add] 挨拶パターンの追加, 挨拶関数の整理

--- src/App.js
+++ src/App.js
@@ -24,11 +24,12 @@ class App extends React.Component {
 
   handleClick(helloType) {
     let koalaSayingList;
+    helloType -= 0;
     this.setState({
       helloType: helloType
     });
     switch (helloType) {
-      case 'おはよう':
+      case 0:
         koalaSayingList = [
           'おはコアラ(*´ω｀*)',
           'おはドアラʕ•͡દ•ʔ ',
@@ -36,15 +37,30 @@ class App extends React.Component {
           'おはにゃん(^·ｪ·^)'
         ];
         break;
-      case 'こんにちは':
+      case 1:
         koalaSayingList = [
-          'こんにちコアラ꒰๑•௰•๑꒱',
+          'いってらコアラ(*´ω｀*)',
+          '今日も頑張れコアラᕙ( ͡° ͜ʖ ͡°)ᕗ',
+          'もう行っちゃうのかコアラ?(°д°)'
+        ];
+        break;
+      case 2:
+        koalaSayingList = [
+          'こんにちコアラ(`·⊝·´ )',
           'お腹すいた…|ω`)',
           'こんちくわ( ·∀·)',
           'コアラのマーチ┗=͟͟͞͞( ˙∀˙)=͟͟͞͞┛'
         ];
         break;
-      case 'おやすみ':
+      case 3:
+        koalaSayingList = [
+          'おつカレーコアラ(*´ω｀*)',
+          '今日もよく頑張ったのだ(❁´ω`❁)',
+          '( ˘ω˘ )つ[ｵﾌﾄｩﾝ]',
+          '( ˘ω˘ )つ[ﾕｰｶﾘ]'
+        ];
+        break;
+      case 4:
         koalaSayingList = [
           'おやすみコアラ(ﾟДﾟ)ﾉ',
           'くそねみコアラ(˘ω˘)',
@@ -53,6 +69,7 @@ class App extends React.Component {
         break;
       default:
         koalaSayingList = null;
+        console.log(helloType);
     }
     
     if (koalaSayingList) {
@@ -69,7 +86,23 @@ class App extends React.Component {
   }
   
   render() {
-    const urlPage = 'https://hello-koala.netlify.com/';
+    let htmlBtns = [];
+    const dataBtn = [
+      'おはよう',
+      'いってきます',
+      'こんにちは',
+      'しごおわ',
+      'おやすみ'
+    ];
+    for (const i in dataBtn) {
+      htmlBtns.push(
+        <li>
+          <button key={i}
+            onClick={() => {this.handleClick(i)}}
+          >{dataBtn[i]}！</button>
+        </li>
+      );
+    }
     let htmlModal;
     if (this.state.isModalOpen) {
       htmlModal = (
@@ -80,41 +113,23 @@ class App extends React.Component {
               <p className="modal-text">「{this.state.koalaSaying}」</p>
             </div>
             <button className="modal-share" onClick={() => {
-              window.open(`https://twitter.com/share?url=${urlPage}&text= コアラに「${this.state.helloType}」と挨拶しました！/コアラ「${this.state.koalaSaying}」/Hello! Koala!`, "SNS_window", "width=600, height=500, menubar=no, toolbar=no, scrollbars=yes");
+              window.open(`https://twitter.com/share?url=${urlPage}&text= コアラに「${dataBtn[this.state.helloType]}」と挨拶しました！/コアラ「${this.state.koalaSaying}」/Hello! Koala!`, "SNS_window", "width=600, height=500, menubar=no, toolbar=no, scrollbars=yes");
             }}>結果をツイートする</button>
             <div className="modal-close" onClick={() => {this.setState({isModalOpen: false});}}>×</div>
           </div>
         </div>
       );
     }
+    const urlPage = 'https://hello-koala.netlify.com/';
+    
     return (
       <div className="App">
         <h1>Hello! Koala!</h1>
         <p>我が家のコアラです。<br />コアラに挨拶しよう！</p>
         <img src={koala} className="app-koala" alt="" />
-        <div className="btn-wrap">
-          <button 
-            onClick={
-              () => {
-                this.handleClick('おはよう')
-              }
-            }
-          >おはよう</button>
-          <button 
-            onClick={
-              () => {
-                this.handleClick('こんにちは')
-              }
-            }
-          >こんにちは</button>
-          <button 
-            onClick={
-              () => {
-                this.handleClick('おやすみ')
-              }
-            }
-          >おやすみ</button>
-        </div>
+        <ul className="btn-wrap">
+          {htmlBtns}
+        </ul>
         {htmlModal}
         <footer className="App-footer">
           <img src={logo} className="App-logo" alt="logo" />
